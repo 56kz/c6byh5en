34f30406bc007efbf4e95a04bce57a7bbb7df6cc
khris <khris56@hotmail.com>
2019-09-02 10:29:57 -0500

    create products with httparty

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,5 +1,20 @@
 class Api::V1::ProductsController < ApplicationController
+  protect_from_forgery with: :null_session
   def index
     render json: Product.all
   end
+
+  def create
+    product = Product.new(product_params)
+    if product.save
+      render json: product
+    else
+      render json: { errors: product.errors }, status: 422
+    end
+  end
+
+  private
+   def product_params
+     params.require(:product).permit(:name, :price)
+   end
 end
